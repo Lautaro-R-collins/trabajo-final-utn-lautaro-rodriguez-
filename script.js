c7d16b41ed995d9f76b560fafc074e4c78af7e52
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2023-07-12 17:37:28 -0300

    updated

---
+++ script.js
@@ -0,0 +1,32 @@
+let video=document.querySelector('video');
+let playBoton = document.getElementById("play");
+let pauseBoton= document.getElementById("pause");
+let muteBoton = document.getElementById("mute");
+
+setTimeout(()=>{document.getElementById('showTime').innerHTML=` Duracion video : ${video.duration.toFixed(2)}`},1000);
+
+for (let i = 0; i < video.tim; i++) {
+    console.log(video.currentTime)   
+}
+
+playBoton.addEventListener('click',()=>{
+    video.play()  
+});
+
+pauseBoton.addEventListener('click',()=>{
+    video.pause();
+    flag=false;
+});
+
+muteBoton.addEventListener("click", function() {
+    if (video.muted) {
+      video.muted = false;
+      
+    } else {
+      video.muted = true;
+      
+    }
+  });
+
+  
+  
